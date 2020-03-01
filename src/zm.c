22ee0b279f7103938fc9a97f4c561305f6bbecaa
Uwe Ohse <uwe@ohse.de>
2020-03-01 22:35:28 +0000

    may-be-security-fix: avoid possible underflow

--- src/zm.c
+++ src/zm.c
@@ -432,10 +432,11 @@ zsdata(const char *buf, size_t length, int frameend)
 	VPRINTF(3,("zsdata: %lu %s", (unsigned long) length, 
 		Zendnames[(frameend-ZCRCE)&3]));
 	crc = 0;
-	do {
+	while (length>0) {
 		zsendline(*buf); crc = updcrc((0377 & *buf), crc);
 		buf++;
-	} while (--length>0);
+		length--;
+	}
 	xsendline(ZDLE); xsendline(frameend);
 	crc = updcrc(frameend, crc);
 
