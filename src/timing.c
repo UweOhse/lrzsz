1204a0b80d6b2eea57d557421940aba837168f74
Uwe Ohse <uwe@ohse.de>
2020-03-08 14:24:42 +0000

    cleanup #ifdef mess

--- src/timing.c
+++ src/timing.c
@@ -25,103 +25,36 @@
 
 #include "timing.h"
 
-#if HAVE_SYS_PARAM_H
-#include <sys/param.h>
-#endif
+#include <sys/time.h>
 
-#if !defined(TIME_WITH_SYS_TIME) && !defined(HAVE_SYS_TIME_H)
-   /* can't use gettimeofday without struct timeval */
-#  undef HAVE_GETTIMEOFDAY
-#endif
-
-/* Prefer gettimeofday to ftime to times.  */
-#if defined(HAVE_GETTIMEOFDAY)
-#  undef HAVE_FTIME
-#  undef HAVE_TIMES
-#else
-#  if defined(HAVE_FTIME)
-#    undef HAVE_TIMES
-#  endif
-#endif
-
-#ifdef HAVE_FTIME
-#  include <sys/timeb.h>
-#endif
-
-#ifdef HAVE_TIMES
-#  if HAVE_SYS_TIMES_H
-#    include <sys/times.h>
-#  endif
-#  ifdef _SC_CLK_TCK
-#    define HAVE_SC_CLK_TCK 1
-#  else
-#    define HAVE_SC_CLK_TCK 0
-#  endif
-/* TIMES_TICK may have been set in policy.h, or we may be able to get
-   it using sysconf.  If neither is the case, try to find a useful
-   definition from the system header files.  */
-#  if !defined(TIMES_TICK) && (!defined(HAVE_SYSCONF) || !defined(HAVE_SC_CLK_TCK))
-#    ifdef CLK_TCK
-#      define TIMES_TICK CLK_TCK
-#    else /* ! defined (CLK_TCK) */
-#      ifdef HZ
-#        define TIMES_TICK HZ
-#      endif /* defined (HZ) */
-#    endif /* ! defined (CLK_TCK) */
-#else
-#  endif /* TIMES_TICK == 0 && (! HAVE_SYSCONF || ! HAVE_SC_CLK_TCK) */
-#  ifndef TIMES_TICK
-#    define TIMES_TICK 0
-#  endif
-#endif /* HAVE_TIMES */
 
 double 
 timing (int reset, time_t *nowp)
 {
-  static double starttime, stoptime;
-  double yet;
-#define NEED_TIME
+	static double starttime;
+	double yet=0.0;
+	int ok = 0;
+
 #ifdef HAVE_GETTIMEOFDAY
-  struct timeval tv;
-  struct timezone tz;
-
-#ifdef DST_NONE
-  tz.tz_dsttime = DST_NONE;
-#else
-  tz.tz_dsttime = 0;
-#endif
-  gettimeofday (&tv, &tz);
-  yet=tv.tv_sec + tv.tv_usec/1000000.0;
-#undef NEED_TIME
+	struct timeval tv;
+	if (0==gettimeofday (&tv, NULL)) {
+		yet=tv.tv_sec + tv.tv_usec/1000000.0;
+		ok=1;
+	}
 #endif
 
-#ifdef NEED_TIME
-	yet=(double) time(NULL);
-#endif
-  if (nowp)
-    *nowp=(time_t) yet;
-  if (reset) {
-    starttime = yet;
-    return starttime;
-  }
-  else {
-    double elaptime;
-    stoptime = yet;
-    elaptime = stoptime - starttime;
-    return elaptime;
-  }
-}
+	if (!ok) {
+		yet=(double) time(NULL);
+	}
 
-/*#define TEST*/
-#ifdef TEST
-main()
-{
-	int i;
-	printf("timing %g\n",timing(1,NULL));
-	printf("timing %g\n",timing(0,NULL));
-	for(i=0;i<20;i++){
-	sleep(1);
-	printf("timing %g\n",timing(0,NULL));
+	if (nowp)
+		*nowp=(time_t) yet;
+	if (reset) {
+		starttime = yet;
+		return starttime;
+	} else {
+		double elaptime;
+		elaptime = yet - starttime;
+		return elaptime;
 	}
 }
-#endif
